c7c9f65b3e7fdb2c51af6073843e1ba93eed94a4
Chris Macklin <macklin@amyris.com>
2016-11-28 16:32:04 -0800

    REQ: update to use newest GslCore

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 0.0.2 - November 28 2016
+* Update to use new GslCore.
+
 #### 0.0.1 - November 22 2016
 * Initial release of command line wrapper.
--- src/Gslc/AssemblyInfo.fs
+++ src/Gslc/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Gslc")>]
 [<assembly: AssemblyProductAttribute("Gslc")>]
 [<assembly: AssemblyDescriptionAttribute("GSL compiler command line application")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Gslc"
     let [<Literal>] AssemblyProduct = "Gslc"
     let [<Literal>] AssemblyDescription = "GSL compiler command line application"
-    let [<Literal>] AssemblyVersion = "0.0.1"
-    let [<Literal>] AssemblyFileVersion = "0.0.1"
+    let [<Literal>] AssemblyVersion = "0.0.2"
+    let [<Literal>] AssemblyFileVersion = "0.0.2"
